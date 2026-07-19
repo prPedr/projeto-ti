2b675647a527b6b60508ee348978ced867f83bb9
prPedr <prpedro025@gmail.com>
2026-07-18 23:43:18 -0300

    Refatoração para usar o Usuário Logado

--- back-end/src/controllers/computadoresController.ts
+++ back-end/src/controllers/computadoresController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express'
 import { criarComputador } from '../services/computadoresService.js'
 
 export const criar = (requisicao: Request, resposta: Response) => {
+  requisicao.body.mestre.cadastrado_por = requisicao.usuarioId
+
   const id = criarComputador(requisicao.body)
   resposta.status(201).json({ sucesso: true, id_equipamento: id })
 }
--- back-end/src/routes/computadoresRoutes.ts
+++ back-end/src/routes/computadoresRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { criar } from '../controllers/computadoresController.js'
+import { autenticar } from '../middlewares/authMiddleware.js'
 
 const rotasComputadores = Router()
 
-rotasComputadores.post('/', criar)
+rotasComputadores.post('/', autenticar, criar)
 
 export default rotasComputadores
