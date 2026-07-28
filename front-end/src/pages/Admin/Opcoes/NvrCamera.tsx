3a72aa1710796e539b80272e04f61c061826a769
prPedr <prpedro025@gmail.com>
2026-07-27 23:26:44 -0300

    Adicionar "Tipo de Interface de Rede" às Opções de NVR/Câmera

--- front-end/src/pages/Admin/Opcoes/NvrCamera.tsx
+++ front-end/src/pages/Admin/Opcoes/NvrCamera.tsx
@@ -3,6 +3,7 @@ import { OpcoesPorTipo } from './OpcoesPorTipo';
 const CATEGORIAS_NVR_CAMERA = [
   { valor: 'MARCA', rotulo: 'Marca' },
   { valor: 'MODELO', rotulo: 'Modelo' },
+  { valor: 'TIPO_INTERFACE', rotulo: 'Tipo de Interface de Rede' },
 ];
 
 export default function NvrCameraOpcoes() {
